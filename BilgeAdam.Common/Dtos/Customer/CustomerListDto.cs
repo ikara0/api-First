85fd43bab7dd55b5cf3ebd157cc59bde087c32dd
Ismail Kara <ismail.kara@bilgeadamboost.com>
2022-05-23 21:13:57 +0300

    client example

--- BilgeAdam.Common/Dtos/Customer/CustomerListDto.cs
+++ BilgeAdam.Common/Dtos/Customer/CustomerListDto.cs
@@ -1,13 +1,28 @@
-﻿namespace BilgeAdam.Common.Dtos.Customer
+﻿using System.Text.Json.Serialization;
+
+namespace BilgeAdam.Common.Dtos.Customer
 {
     public class CustomerListDto
     {
+        [JsonPropertyName("id")]
         public string Id { get; set; }
+
+        [JsonPropertyName("companyName")]
         public string CompanyName { get; set; }
+
+        [JsonPropertyName("contactName")]
         public string ContactName { get; set; }
+
+        [JsonPropertyName("contacTitle")]
         public string ContactTitle { get; set; }
+
+        [JsonPropertyName("address")]
         public string Address { get; set; }
+
+        [JsonPropertyName("phone")]
         public string Phone { get; set; }
+
+        [JsonPropertyName("fax")]
         public string Fax { get; set; }
     }
 }
